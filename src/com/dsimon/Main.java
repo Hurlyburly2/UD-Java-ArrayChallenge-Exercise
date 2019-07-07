75b3bfc6a0366f66d7b0757bc43f3d3bb27fe0ef
Hurlyburly2 <dougdougmann@gmail.com>
2019-07-07 04:33:53 -0400

    challenge done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -1,8 +1,52 @@
 package com.dsimon;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        int[] array = getIntegers(5);
+
+        printIntegers(array);
+
+        array = sortIntegers(array);
+
+        printIntegers(array);
+    }
+
+    public static int[] getIntegers(int arrayLength) {
+        System.out.println("Enter " + arrayLength + " integers:");
+        int[] array = new int[arrayLength];
+
+        for (int i = 0; i < array.length; i++ ) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printIntegers(int[] array) {
+        for (int i = 0; i < array.length; i++ ) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int number = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = number;
+                    sorted = false;
+                }
+
+            }
+        }
+
+        return array;
     }
 }
